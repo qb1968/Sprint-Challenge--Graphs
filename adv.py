e17d0fe93186defc7d043589413cf6a6a6dcb7e0
qb1968 <qb1968@aol.com>
2020-06-12 12:55:53 -0400

    MVP, I Hope

--- adv.py
+++ adv.py
@@ -1,7 +1,7 @@
 from room import Room
 from player import Player
 from world import World
-
+from util import Stack
 import random
 from ast import literal_eval
 
@@ -9,11 +9,12 @@ from ast import literal_eval
 world = World()
 
 
+
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
-# map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
+map_file = "maps/test_line.txt"
+map_file = "maps/test_cross.txt"
+map_file = "maps/test_loop.txt"
+map_file = "maps/test_loop_fork.txt"
 map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
@@ -29,7 +30,46 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+def get_directions(current_room, visited_rooms):
+    exits = []
 
+    for exit in current_room.get_exits():
+        if room_graph[current_room.id][1][exit] not in visited_rooms:
+            exits.append(exit)
+    return exits
+
+def find_room():
+    visited_rooms = set()
+    visited_rooms.add(player.current_room.id)
+    path = []
+
+    
+    while len(visited_rooms) < len(room_graph.keys()):
+        current_room = player.current_room.id
+        exits = get_directions(player.current_room, visited_rooms)
+
+        if len(exits) == 0: 
+            exit_path = path.pop()
+            player.travel(exit_path) 
+            traversal_path.append(exit_path) 
+            continue 
+
+        for exit_path in exits:
+            visited_rooms.add(room_graph[current_room][1][exit_path]) 
+            traversal_path.append(exit_path)  
+
+            if exit_path == "n":
+                path.append("s")
+            elif exit_path == "s":
+                path.append("n")
+            elif exit_path == "w":
+                path.append("e")
+            else:
+                path.append("w")
+            player.travel(exit_path)
+            break
+
+find_room()
 
 # TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
